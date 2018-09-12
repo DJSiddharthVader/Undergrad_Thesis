6b306de4565b153e78224d3abdea8ead0fd1a87f
Sid Reed <sidreed34@gmail.com>
2018-09-12 19:16:52 -0400

    added comments

--- scripts/get_gbffs.sh
+++ scripts/get_gbffs.sh
@@ -2,32 +2,10 @@
 
 #file to download gbffs from ncbi summary txt file
 
-# 1 assembly_accession
-# 2 bioproject
-# 3 biosample
-# 4 wgs_master
-# 5 refseq_category
-# 6 taxid
-# 7 species_taxid
-# 8 organism_name
-# 9 infraspecific_name
-# 10 isolate
-# 11 version_status
-# 12 assembly_level
-# 13 release_type
-# 14 genome_rep
-# 15 seq_rel_date
-# 16 asm_name
-# 17 submitter
-# 18 gbrs_paired_asm
-# 19 paired_asm_comp
-# 20 ftp_path
-# 21 excluded_from_refseq
-# 22 relation_to_type_material
-ftpurl="ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt"
+ftpurl="ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt" #tsv-ish of bacteria, accs, names and ftp urls for everything in refseq
 rawsummary="assembly_summary_110918SR.txt"
-filesuffix="_genomic.gbff.gz"
-outfile="all_refseq_bacteria_ftp_paths_110918.txt"
+filesuffix="_genomic.gbff.gz" #file suffix for every organism
+outfile="all_refseq_bacteria_ftp_paths_110918.txt" #outputfile with all ftp links
 
 wget -qO- $ftpurl | tee "$rawsummary" | sed 's/\t\{1,\}/,/g' | awk -F "," '{print $0 ~ "latest" && $0 ~ "Complete Genome"?$(NF-1):""}' | sed '/^$/d' | perl -pe 's/(\/[^\/]+?)$/\1\1/g' | sed "s/$/$filesuffix/g" >| "$outfile"
 echo "Got all paths in $outfile"
@@ -36,3 +14,13 @@ for url in $(cat "$outfile");
 do
     wget $url
 done
+
+
+#wget -qO- $ftpurl #get file from ncbi, quiet, direct to stdout
+#tee "$rawsummary" #write file to file, continues to pass through stdout
+#sed 's/\t\{1,\}/,/g' #sub tabs, multitabs for commas
+#awk -F "," '{print $0 ~ "latest" && $0 ~ "Complete Genome"?$(NF-1):""}' #for rows where the genome is complete and the latest, print the 2nd last column (ftp link for organism) else print nothing
+#sed '/^$/d' # remove blank lines
+#perl -pe 's/(\/[^\/]+?)$/\1\1/g' #fix links so they point to specific file name without suffix
+#sed "s/$/$filesuffix/g" #add suffix to the end of each ftp link (line)
+## >| "$outfile" # write to output file specified above

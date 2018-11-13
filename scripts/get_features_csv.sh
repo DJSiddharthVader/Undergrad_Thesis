31c288b4742edd2a4d9aa333abea7559db9a3de4
Sid Reed <sidreed34@gmail.com>
2018-11-13 14:27:08 -0500

    added GCA number, still not parallelized

--- scripts/get_features_csv.sh
+++ scripts/get_features_csv.sh
@@ -6,6 +6,7 @@
 declare -a gcounts=("Genes (total)" "CDS (total)" "Genes (coding)" "CDS (coding)" "Genes (RNA)" "tRNAs" "ncRNAs" "Pseudo Genes (total)")
 singlefileinfo() {  #get info for a single gbfffile, , delimited
     #identifying info
+    local GCA=$(grep -m 1 'Assembly' "$1" | cut -d':' -f2 | tr -d ' ') #gca for linking back to crispr annoattions
     local accnum=$(head -1 "$1" | sed 's/^\w\+\s\+\(\w\+\)\s.*$/\1/g') #accession
     local orgname=$(grep -m 1 ORGANISM "$1" | sed 's/^\s\+ORGANISM\s\+//') #name
     local genus=$(echo $orgname | perl -pe 's/^(\w.*?)\s.*$/\1/') #genus only
@@ -15,7 +16,7 @@ singlefileinfo() {  #get info for a single gbfffile, , delimited
     if [[ $crisprs == '' ]];then
         crisprs=0
     fi
-    info=("$accnum" "$orgname" "$genus" "$taxonomy" "$complet" "$crisprs" )
+    info=("$GCA" "$accnum" "$orgname" "$genus" "$taxonomy" "$complet" "$crisprs" )
     #gene counts
     for gcount in "${gcounts[@]}" #get gene type counts for bacteria
     do
@@ -32,7 +33,7 @@ singlefileinfo() {  #get info for a single gbfffile, , delimited
     echo "$outvar"
 }
 
-headers="Filepath~Accession Number~Organism~Genus~Taxonomy~IsComplete~NCBI_CRISPR_Arrays~Genes (total)~CDS (total)~Genes (coding)~CDS (coding)~Genes (RNA)~tRNAs~ncRNAs~Pseudo Genes (total)"
+headers="Filepath~GCA~Accession Number~Organism~Genus~Taxonomy~IsComplete~NCBI_CRISPR_Arrays~Genes (total)~CDS (total)~Genes (coding)~CDS (coding)~Genes (RNA)~tRNAs~ncRNAs~Pseudo Genes (total)"
 
 echo "$headers"
 for file in $1* #dir of gbff files

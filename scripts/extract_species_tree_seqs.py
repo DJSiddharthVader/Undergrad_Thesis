90a1c6d99ba26e62ab04c26a5153546ff3b93361
Sid Reed <sidreed34@gmail.com>
2018-11-13 11:13:09 -0500

    need to rename gene fasta to have taxa as headers so nexus aln can be correctly combined, redoing some test stuff

--- scripts/extract_species_tree_seqs.py
+++ scripts/extract_species_tree_seqs.py
@@ -50,7 +50,7 @@ def writeAllSpeciesTreeFastas(pamat,columnindex,familyindex,nucFasta,outdir,proc
     os.system('mkdir -p {}'.format(outdir))
     familyidxlist = getGeneFamilies(pamat,columnindex)
     familylist = familyToList(familyidxlist,familyindex,fams)
-    if len(os.listdir(outdir)) == len(familylist): #previously cached results
+    if len(os.listdir(outdir)) == fams: #previously cached results
         return outdir
     pool = ThreadPool(processes)
     parlistToFasta = partial(listToFasta,fastaname=nucFasta,outdir=outdir)
@@ -58,49 +58,38 @@ def writeAllSpeciesTreeFastas(pamat,columnindex,familyindex,nucFasta,outdir,proc
         pass
     return outdir
 
-def nexusAlnSTreeFastas(sTreeDir):
+def nexusAlnSTreeFastas(sTreeDir,fams):
     faadir = 'aligned_faa_{}'.format(sTreeDir)
     os.system('mkdir -p {}'.format(faadir))
-    mafftbase = 'mafft --quiet --localpair --maxiterate 1000'
-    for fasta in tqdm(os.listdir(sTreeDir),desc='alining'):
-        if fasta.endswith('.fna'):
-            fout = fasta.strip('.fna')
-            mafft ='{} {}/{} > {}/{}.aln'.format(mafftbase,sTreeDir,fasta,faadir,fout)
-            os.system(mafft)
+    if len(os.listdir(faadir)) != fams:
+        mafftbase = 'mafft --quiet --localpair --maxiterate 1000'
+        for fasta in tqdm(os.listdir(sTreeDir),desc='alining'):
+            if fasta.endswith('.fna'):
+                fout = fasta.strip('.fna')
+                mafft ='{} {}/{} > {}/{}.aln'.format(mafftbase,sTreeDir,fasta,faadir,fout)
+                os.system(mafft)
     nexDir = 'aligned_nex_{}'.format(sTreeDir)
     os.system('mkdir -p {}'.format(nexDir))
-    for aln in tqdm(os.listdir(faadir),desc='to nexus'):
-        AlignIO.convert('{}/{}'.format(faadir,aln),'fasta','{}/{}.nex'.format(nexDir,aln.strip('.aln')),'nexus',alphabet=Alphabet.generic_dna)
-#            with open('{}/{}.aln'.format(faadir,fout),'r') as fna, open('{}/{}.nex'.format(nexDir,fout),'w') as nex:
-#                alignment = AlignIO.parse(fna,'fasta')
-#                nexus = AlignIO.write(nex,'nexus')
+    if len(os.listdir(nexDir)) != fams:
+        for aln in tqdm(os.listdir(faadir),desc='to nexus'):
+            AlignIO.convert('{}/{}'.format(faadir,aln),'fasta','{}/{}.nex'.format(nexDir,aln.strip('.aln')),'nexus',alphabet=Alphabet.generic_dna)
     return nexDir
 
-def checkTaxa(matrices):#from https://biopython.org/wiki/Concatenate_nexus
-    """Verify Nexus instances have the same taxa information.
-    Checks that nexus instances in a list [(name, instance)...] have
-    the same taxa, provides useful error if not and returns None if
-    everything matches"""
-    first_taxa = matrices[0][1].taxlabels
-    for name, matrix in matrices[1:]:
-        first_only = [t for t in first_taxa if t not in matrix.taxlabels]
-        new_only = [t for t in matrix.taxlabels if t not in first_taxa]
-        if first_only:
-            missing = ', '.join(first_only)
-            msg='{} taxa {} not in martix {}'.format(nexi[0][0],missing,name)
-            raise Nexus.NexusError(msg)
-        elif new_only:
-            missing = ', '.join(new_only)
-            msg = '{} taxa {} not in all matrices'.format(name, missing)
-            raise Nexus.NexusError(msg)
-    return None # will only get here if it hasn't thrown an exception
+def checkTaxa(nexi):
+    alltaxa = set([y for x in nexi for y in x[1].taxlabels])
+    print(alltaxa)
+    print(len(alltaxa))
+    print([len(x[1].taxlabels) for x in nexi])
+    missingTaxa = [False if set(x[1].taxlabels) == alltaxa else True for x in nexi]
+    print(set(missingTaxa))
+    return False
 
 def concatNexAlns(nexDir,outname,same_taxa=True):#from https://biopython.org/wiki/Concatenate_nexus
     """Combine multiple nexus data matrices in one partitioned file.
     By default this will only work if the same taxa are present in each file
     use same_taxa=False if you are not concerned by this """
     filelist = [x for x in os.listdir(nexDir) if x.endswith('.nex')]
-    nexi = [(fname, Nexus.Nexus(fname)) for fname in filelist]
+    nexi = [(os.path.join(nexDir,fname), Nexus.Nexus(os.path.join(nexDir,fname))) for fname in filelist]
     coutname = 'concat_stree_aln_{}.nex'.format(outname)
     if same_taxa:
         if not checkTaxa(nexi):
@@ -116,7 +105,7 @@ def main(pamat,columnindex,familyindex,nucFasta,outname,processes,fams):
     fastadir = 'species_tree_fastas_{}'.format(outname)
     fastadir = writeAllSpeciesTreeFastas(pamat,columnindex,familyindex,nucFasta,fastadir,processes,fams)
     print('fasta files for species tree genes written to {}'.format(fastadir))
-    nexDir = nexusAlnSTreeFastas(fastadir)
+    nexDir = nexusAlnSTreeFastas(fastadir,fams)
     print('fasta alingments for species tree written to {}'.format(fastadir))
     print('nexus alingments for species tree written to {}'.format(nexDir))
     coutname = concatNexAlns(nexDir,outname,same_taxa=True)
@@ -131,3 +120,23 @@ if __name__ == '__main__':
     processes = 8
     fams = 50
     main(pamat,columnindex,familyindex,nucFasta,outname,processes,fams)
+
+
+def checkTaxa(nexi):#from https://biopython.org/wiki/Concatenate_nexus
+    """Verify Nexus instances have the same taxa information.
+    Checks that nexus instances in a list [(name, instance)...] have
+    the same taxa, provides useful error if not and returns None if
+    everything matches"""
+    first_taxa = nexi[0][1].taxlabels
+    for name, matrix in nexi[1:]:
+        first_only = [t for t in first_taxa if t not in matrix.taxlabels]
+        new_only = [t for t in matrix.taxlabels if t not in first_taxa]
+        if first_only:
+            missing = ', '.join(first_only)
+            msg='{} taxa {} not in martix {}'.format(nexi[0][0],missing,name)
+            raise Nexus.NexusError(msg)
+        elif new_only:
+            missing = ', '.join(new_only)
+            msg = '{} taxa {} not in all matrices'.format(name, missing)
+            raise Nexus.NexusError(msg)
+    return None # will only get here if it hasn't thrown an exception

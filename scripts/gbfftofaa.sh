4274976b1609582f48d232afc0ebe2761aceca00
Sid Reed <sidreed34@gmail.com>
2018-11-19 21:55:26 -0500

    now converts to nuc and prot data

--- scripts/gbfftofaa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-outdir=$(basename "$1" | cut -d'.' -f1)
-mkdir -p $outdir
-while read file;
-do
-     ~/thesis_SidReed/scripts/modified_get_features.pl -a -t "$file" >| "$outdir""/"`basename $file`".faa" 2>> "$outdir""/errors.txt"
-done < $1

ad26618a7db97742824be0008f024a8dd6bfde69
Sid Reed <sidreed34@gmail.com>
2018-12-03 14:28:06 -0500

    was going to download more genomes, no sure yet

--- scripts/get_gbffs.sh
+++ scripts/get_gbffs.sh
@@ -3,14 +3,14 @@
 #file to download gbffs from ncbi summary txt file
 
 #ftpurl="ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt" #tsv-ish of bacteria, accs, names and ftp urls for everything in refseq
-#rawsummary="assembly_summary_110918SR.txt"
 #filesuffix="_genomic.gbff.gz" #file suffix for every organism
 #outfile="all_refseq_bacteria_ftp_paths_110918.txt" #outputfile with all ftp links
-#wget -qO- $ftpurl | tee "$rawsummary" | sed 's/\t\{1,\}/,/g' | awk -F "," '{print $0 ~ "latest" && $0 ~ "Complete Genome"?$(NF-1):""}' | sed '/^$/d' | perl -pe 's/(\/[^\/]+?)$/\1\1/g' | sed "s/$/$filesuffix/g" >| "$outfile"
+summary="/home/sid/thesis_SidReed/bacterialGBFFs/pathfiles/assembly_summary.txt"
 
-filesuffix="_genomic.gbff.gz" #file suffix for every organism
+#filesuffix="_genomic.gbff.gz" #file suffix for every organism
+filesuffix="_genomic.gbk.gz" #file suffix for every organism
 outfile="$2"
-cat "$1" | perl -pe 's/(\/[^\/]+?)$/\1\1/g' | sed "s/$/$filesuffix/g" >| "$outfile"
+cat "$summary" | perl -pe 's/(\/[^\/]+?)$/\1\1/g' | sed "s/$/$filesuffix/g" >| "$outfile"
 echo "Got all paths in $outfile"
 for url in $(cat "$outfile");
 do
@@ -18,6 +18,7 @@ do
 done
 
 
+#wget -qO- $ftpurl | tee "$rawsummary" | sed 's/\t\{1,\}/,/g' | awk -F "," '{print $0 ~ "latest" && $0 ~ "Complete Genome"?$(NF-1):""}' | sed '/^$/d' | perl -pe 's/(\/[^\/]+?)$/\1\1/g' | sed "s/$/$filesuffix/g" >| "$outfile"
 #wget -qO- $ftpurl #get file from ncbi, quiet, direct to stdout
 #tee "$rawsummary" #write file to file, continues to pass through stdout
 #sed 's/\t\{1,\}/,/g' #sub tabs, multitabs for commas

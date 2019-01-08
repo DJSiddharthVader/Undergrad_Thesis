87093c3342e39487de5775132fac0897bf4bfcb7
Sid Reed <sidreed34@gmail.com>
2019-01-08 10:06:20 -0500

    rewrote everything to be compatible with gene tree generator, parallel, lot cleaner, works now, need to add cli args

--- scripts/build_species_tree.py
+++ scripts/build_species_tree.py
@@ -8,7 +8,8 @@ import pandas as pd
 from tqdm import tqdm
 from Bio.Nexus import Nexus
 from functools import partial
-from Bio import SeqIO,AlignIO,Alphabet
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO,AlignIO,Phylo,Alphabet
 from multiprocessing.dummy import Pool as ThreadPool
 
 #arg 1 is PA matrix (not binary)
@@ -19,17 +20,12 @@ from multiprocessing.dummy import Pool as ThreadPool
 
 #will extract all genes present in only 1 copy in every taxa and spit to a fasta file for alignment and tree creation
 
-def matrix_to_binary(pamat):
-    binarize = lambda x:np.where(x > 0, 1, 0)
-    vecbin = np.vectorize(binarize)
-    return vecbin(pamat)
-
-def pickGeneFamilies(pamat,columnindex):
-    binmat = matrix_to_binary(pamat)
-    sizes = np.apply_along_axis(sum,0,binmat)
-    family_idxs = [str(i) for i,size in enumerate(sizes) if size == binmat.shape[0]]
-    print('using {} of {} genes for species tree'.format(len(family_idxs),binmat.shape[1]))
-    return family_idxs
+def pickGeneFamilies(pamat,columnindex,genes):
+    sizes = np.apply_along_axis(sum,0,pamat)
+    family_idxs = [str(i) for i,pavec in enumerate(pamat.T) if np.array_equal(pavec,np.ones(pamat.shape[0]))]
+    print('using {} of {} usable of {} total genes for species tree'.format(genes,len(family_idxs),pamat.shape[1]))
+    #usable means that the gene family has exactly 1 gene in each organism
+    return family_idxs[:genes]
 
 def extractSeqsForTree(headerlist,nucFasta,outdir,genefam):
     seqRecordlist = []
@@ -40,8 +36,15 @@ def extractSeqsForTree(headerlist,nucFasta,outdir,genefam):
 
 def fixNames(seqlist):
     fixedseqs = []
-    for sequence in seqlist:
-        fixedseqs.append(SeqRecord(sequence.seq, name=sequence.name.split(':')[0], id=sequence.id.split(':')[0]))
+    found_taxa = []
+    for cseq in seqlist:
+        new_name = cseq.name.split(':')[0]
+        if not new_name in found_taxa:
+            fixedseqs.append(SeqRecord(cseq.seq,
+                                       name=new_name,
+                                       id=new_name,
+                                       description=''))
+            found_taxa.append(new_name)
     return fixedseqs
 
 def writeFastas(nameseqiter):
@@ -50,73 +53,84 @@ def writeFastas(nameseqiter):
             SeqIO.write(seqs,outfile,'fasta') #write all seqs to a fasta file
     return None
 
-def concatNexAlns(nexDir,outname,same_taxa=True):#from https://biopython.org/wiki/Concatenate_nexus
+def alignGeneFamilies(fastafile,outdir):
+    mafftbase = 'mafft --quiet --localpair --maxiterate 1000'
+    fastabase = fastafile.split('.')[0]
+    fastaname = '{}/fastas/{}'.format(outdir,fastafile)
+    alnfile = '{}/tmp_{}.aln'.format(outdir,fastabase)
+    align = '{} {} > {}'.format(mafftbase,fastaname,alnfile)
+    os.system(align)
+    AlignIO.convert(alnfile,'fasta','{}/nexus/{}.nex'.format(outdir,fastabase),'nexus',alphabet=Alphabet.generic_dna)
+    os.remove(alnfile)
+    return None
+
+def parallelAlignFastas(outdir,processes):
+    pool = ThreadPool(processes)
+    fastas = os.listdir('{}/fastas'.format(outdir))
+    alnfnc = partial(alignGeneFamilies,outdir=outdir)
+    alnfiles = list(tqdm(pool.imap(alnfnc,fastas),total=len(fastas),desc='aligning'))
+    return None
+
+def concatNexAlns(outdir,genusname):
     """Combine multiple nexus data matrices in one partitioned file.
     By default this will only work if the same taxa are present in each file
     use same_taxa=False if you are not concerned by this """
-    filelist = [x for x in os.listdir(nexDir) if x.endswith('.nex')]
-    nexi = [(os.path.join(nexDir,fname), Nexus.Nexus(os.path.join(nexDir,fname))) for fname in filelist]
-    coutname = 'concat_stree_aln_{}.nex'.format(outname)
+    nexdir = '{}/nexus/'.format(outdir)
+    filelist = [x for x in os.listdir(nexdir) if x.endswith('.nex')]
+    nexi = [(os.path.join(nexdir,fname), Nexus.Nexus(os.path.join(nexdir,fname))) for fname in filelist]
+    coutname = '{}/concat_aln_species_tree_{}.nex'.format(outdir,genusname)
     combined = Nexus.combine(nexi)
     combined.write_nexus_data(filename=open(coutname,'w'))
     return coutname
 
-def buildMbSpecTree(nexaln,outname):
+def buildSpeciesTree(concat_nexus_aln,outdir,genusname):
     mbf =\
 """set autoclose=yes nowarn=yes
 execute {}
 lset nst=6 rates=gamma
-mcmc ngen=10000 savebrlens=yes file={}_{}
-quit""".format(nexaln,'spectree',outname)
-    mbscriptname = 'mbScript_{}.txt'.format(outname)
+mcmc ngen=10000 savebrlens=yes file=tmp_species_tree_{}
+quit""".format(concat_nexus_aln,genusname)
+    mbscriptname = 'tmp_mrbayes_script.txt'
     open(mbscriptname,'w').write(mbf)
-    logfilename = 'mb_spectree_log_{}.txt'.format(outname)
+    logfilename = 'tmp_mrbayes_species_tree_log.txt'
     with open(mbscriptname,'rb',0) as mbscript, open(logfilename,'wb',0) as logfile:
         logtxt = subprocess.run(['mb'],
                                 stdin=mbscript,
                                 stdout=logfile,
                                 check=True)
-    outdir = 'mb_species_tree_{}'.format(outname)
-    os.system('mkdir -p {}'.format(outdir))
-    for fp in glob.glob('./*spectree*'):
-        os.rename(fp,os.path.join(os.getcwd(),outdir,fp))
-    os.rename(mbscriptname,os.path.join(os.getcwd(),outdir,mbscriptname))
-    return outdir
+    treedir= '{}/species_tree_{}'.format(outdir,genusname)
+    os.system('mkdir -p {}'.format(treedir))
+    os.rename(mbscriptname,os.path.join(os.getcwd(),treedir,mbscriptname))
+    for fp in glob.glob('./*tmp_*'):
+        os.rename(fp,os.path.join(os.getcwd(),treedir,fp.replace('tmp_','')))
+    return treedir
 
-def main(pamat,columns,familys,nucFasta,genusname,genes):
+def convertToNewick(outdir,treedir):
+    treefile = glob.glob('{}/*run2.t'.format(treedir))[0]
+    for tree in Phylo.parse(treefile,'nexus'):
+        Phylo.write(tree,'{}/trees_newick/{}_species_tree_{}.nwk'.format(outdir,genusname,tree.name),'newick')
+    return None
+
+def main(pamat,columns,familys,nucFasta,genusname,genes,processes):
     #set up out directories
     outdir = 'species_tree_files'
     os.system('mkdir -p {}'.format(outdir))
     os.system('mkdir -p {}/fastas'.format(outdir))
     os.system('mkdir -p {}/nexus'.format(outdir))
-    os.system('mkdir -p {}/trees'.format(outdir))
-    #pick families
-    family_col_idxs = pickGeneFamiliesForTrees(pamat,columns,minsize)[:genes]
+    print('picking gene families for species tree...')
+    family_col_idxs = pickGeneFamilies(pamat,columns,genes)
     family_idxs = [columns[col_idx] for col_idx in family_col_idxs]
     headerlists = {famidx:familys[famidx] for famidx in family_idxs}
-    #write families to fasta files
+    print('writing gene families to fastas...')
     seqs_to_write = [extractSeqsForTree(heads,nucFasta,outdir,fam) for fam,heads in tqdm(headerlists.items(),total=len(list(headerlists.keys())),desc='getseqs')]
     seqs_to_write = {name:fixNames(seqs) for (name,seqs) in seqs_to_write}
     writeFastas(seqs_to_write)
-    #align gene family fastas and convert to nexus
-    alignGeneFamilies(outdir)
-    #buiil gene trees with mrbayes from alingments
-    buildTrees(outdir)
+    print('aligning and concatenating genes...')
+    parallelAlignFastas(outdir,processes)
+    concatfilename = concatNexAlns(outdir,genusname)
+    print('building species tree with concatenated alingment...')
+    treedir = buildSpeciesTree(concatfilename,outdir,genusname)
     return None
-
-def main(pamat,columnindex,familyindex,nucFasta,outname,processes,fams):
-    fastadir = 'species_tree_fastas_{}'.format(outname)
-    fastadir,treegenes = writeAllSpeciesTreeFastas(pamat,columnindex,familyindex,nucFasta,fastadir,processes,fams)
-    print('{} genes used for species tree'.format(treegenes))
-    print('fasta files for species tree written to {}'.format(fastadir))
-    nexDir = nexusAlnSTreeFastas(fastadir,fams)
-    print('fasta alingments for species tree written to {}'.format(fastadir))
-    print('nexus alingments for species tree written to {}'.format(nexDir))
-    coutname = concatNexAlns(nexDir,outname,same_taxa=True)
-    print('concat nexus aln file for mrbayes written to {}'.format(coutname))
-    print('running MrBayes ...')
-    toutname = buildMbSpecTree(coutname,outname)
-    print('mrbayes species tree files in {}'.format(toutname))
 
 if __name__ == '__main__':
     pamat = np.load(sys.argv[1])
@@ -124,5 +138,6 @@ if __name__ == '__main__':
     famidx = json.load(open(sys.argv[3]))
     nucFasta = sys.argv[4]
     genusname = sys.argv[5]
-    minsize = 0.3
-    main(pamat,colidx,famidx,nucFasta,genusname,minsize)
+    genes_for_species_tree = 50
+    processes = 8
+    main(pamat,colidx,famidx,nucFasta,genusname,genes_for_species_tree,processes)

8150c3f73f9e4474d173c22d1c535f5592e80ffa
Sid Reed <sidreed34@gmail.com>
2019-01-17 13:13:46 -0500

    reogranized

--- scripts/prepSeqs.sh
+++ scripts/prepSeqs.sh
@@ -9,29 +9,6 @@ extractFastas() {
     done < "$1"
 }
 
-extractMixedFastas() {
-    mdir="mix_$2"
-    mkdir -p "$mdir"
-    while read file; do
-        ~/thesis_SidReed/scripts/modified_get_features.pl -b -t "$file" >| "$mdir/`basename $file | cut -d'.' -f-2`.fnaa" 2>> "$mdir/errors.txt"
-    done < "$1"
-}
-
-seperateMixedFastas() {
-    pdir="prot_$1"
-    mkdir -p "$pdir"
-    ndir="nuc_$1"
-    mkdir -p "$ndir"
-    for file in "mix_$1"/*.fnaa; do
-        python ~/thesis_SidReed/scripts/seperateFasta.py "$file" >| tmp_fnames
-        base=`basename $file | cut -d'.' -f-2`
-        protname=`tail -1 tmp_fnames`
-        mv "$protname" "./$pdir/$base".faa
-        nucname=`head -1 tmp_fnames`
-        mv "$nucname" "./$ndir/$base".fna
-    done
-}
-
 dedupFasta() {
     sed -e '/^>/s/$/@/' -e 's/^>/#/' "$1" |\
         tr -d '\n' | tr "#" "\n" | tr "@" "\t" |\
@@ -61,6 +38,35 @@ main() {
     cd -
 }
 
+#need conda environment activated, cant do it in script for some reason so just make sure its activated first
+#$1 is paths to gbff files, $2 is genus name
+shopt -s extglob #allows for globbing in scripts
+main "$1" "$2"
+
+#DEPRECIATED
+extractMixedFastas() {
+    mdir="mix_$2"
+    mkdir -p "$mdir"
+    while read file; do
+        ~/thesis_SidReed/scripts/modified_get_features.pl -b -t "$file" >| "$mdir/`basename $file | cut -d'.' -f-2`.fnaa" 2>> "$mdir/errors.txt"
+    done < "$1"
+}
+
+seperateMixedFastas() {
+    pdir="prot_$1"
+    mkdir -p "$pdir"
+    ndir="nuc_$1"
+    mkdir -p "$ndir"
+    for file in "mix_$1"/*.fnaa; do
+        python ~/thesis_SidReed/scripts/seperateFasta.py "$file" >| tmp_fnames
+        base=`basename $file | cut -d'.' -f-2`
+        protname=`tail -1 tmp_fnames`
+        mv "$protname" "./$pdir/$base".faa
+        nucname=`head -1 tmp_fnames`
+        mv "$nucname" "./$ndir/$base".fna
+    done
+}
+
 main2() {
     extractMixedFastas "$1" "$2"
     seperateMixedFastas "$2"
@@ -72,11 +78,3 @@ main2() {
     rmEmptyHeads all_nucleotides.fna
 
 }
-
-shopt -s extglob #allows for globbing in scripts
-#main "$1" "$2"
-#need conda environment activated, cant do it in script for some reason so just make sure its activated first
-#$1 is paths to gbff files, $2 is genus name
-main "$1" "$2"
-
-

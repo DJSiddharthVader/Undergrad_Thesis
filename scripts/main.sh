f8d1db153a6b3ffdd276acb9d25c69dc2212d169
Sid Reed <sidreed34@gmail.com>
2019-01-25 17:44:00 -0500

    switching to mmseqs for clustering, changed accordingly

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 scriptdir="/home/sid/thesis_SidReed/scripts"
-fps="$1"
-genusname="$2"
+fps="$1" #file with abs paths to all genomes being used
+genusname="$2" #genus name
 echo "preping sequences"
 "$scriptdir"/prepSeqs.sh "$fps" "$genusname"
-echo "all vs all alingment"
-"$scriptdir"/diamondAlign.sh "$genusname"
-cd "$genusname"
 echo "single link clustering to gene families"
-python "$scriptdir"/single_link_clustering_blast_hits.py allvsallproteins.dmnd.out all_proteins.faa "$genusname"
+cd "$genusname"
+mkdir mmseq_clustering
+cd mmseq_clustering
+"$scriptdir"/mmseqWrapper.sh ../all_proteins.faa "$genusname"
+cd -
+python "$scriptdir"/parse_mmseq_clusters.py ./mmseq_clustering/clusters_"$genusname".tsv ./all_proteins.faa
 echo "creating PA matrix"
-python "$scriptdir"/createPAMatrix.py gene_families_"$genusname".json protein_fastas/ "$genusname"
+python "$scriptdir"/createPAMatrix.py gene_families.json protein_fastas/ "$genusname"
 echo "building species trees"
-python "$scriptdir"/build_species_tree.py pa_matrix_"$genusname".npy column_indexes_families_"$genusname".json gene_families_"$genusname".json all_nucleotides.fna "$genusname"
+python "$scriptdir"/build_species_tree.py pa_matrix.npy column_indexes_families.json gene_families.json all_nucleotides.fna "$genusname"
 echo "building gene trees"
-python "$scriptdir"/generate_gene_trees.py pa_matrix_"$genusname".npy column_indexes_families_"$genusname".json gene_families_"$genusname".json all_nucleotides.fna "$genusname"
+python "$scriptdir"/generate_gene_trees.py pa_matrix.npy column_indexes_families.json gene_families.json all_nucleotides.fna "$genusname"
 python "$scriptdir"/build_network_from_trees.py ../"$genusname"
+#old main
+#echo "preping sequences"
+#"$scriptdir"/prepSeqs.sh "$fps" "$genusname"
+#echo "all vs all alingment"
+#"$scriptdir"/diamondAlign.sh "$genusname"
+#cd "$genusname"
+#echo "single link clustering to gene families"
+#python "$scriptdir"/single_link_clustering_blast_hits.py allvsallproteins.dmnd.out all_proteins.faa "$genusname"
+#echo "creating PA matrix"
+#python "$scriptdir"/createPAMatrix.py gene_families.json protein_fastas/ "$genusname"
+#echo "building species trees"
+#python "$scriptdir"/build_species_tree.py pa_matrix_"$genusname".npy column_indexes_families_"$genusname".json gene_families_"$genusname".json all_nucleotides.fna "$genusname"
+#echo "building gene trees"
+#python "$scriptdir"/generate_gene_trees.py pa_matrix_"$genusname".npy column_indexes_families_"$genusname".json gene_families_"$genusname".json all_nucleotides.fna "$genusname"
+#python "$scriptdir"/build_network_from_trees.py ../"$genusname"

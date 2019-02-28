39621cd34632ed6f1fb6a92d125724181f6107e2
Sid Reed <sidreed34@gmail.com>
2019-02-28 10:12:00 -0500

    tried creating mixed fasta and seperating, but still results in unequal nuc/prot fasta, going to download the fastas directly

--- scripts/prepSeqs.sh
+++ scripts/prepSeqs.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-extractFastas() {
-    mkdir -p "$2"
-    mkdir -p "$3"
-    while read file; do
-        ~/thesis_SidReed/scripts/modified_get_features.pl -a -t "$file" >| "$2""/"`basename $file`".faa" 2>> "$2""/errors.txt"
-        ~/thesis_SidReed/scripts/modified_get_features.pl -c -t "$file" >| "$3""/"`basename $file`".fna" 2>> "$3""/errors.txt"
-    done < "$1"
-}
-
 dedupFasta() {
     sed -e '/^>/s/$/@/' -e 's/^>/#/' "$1" |\
         tr -d '\n' | tr "#" "\n" | tr "@" "\t" |\
@@ -21,6 +12,15 @@ dedupFasta() {
 rmEmptyHeads (){
     awk 'BEGIN {RS = ">" ; FS = "\n" ; ORS = ""} $2 {print ">"$0}' "$1" > tmp_rmempt
     mv tmp_rmempt "$1"
+}
+
+extractFastas() {
+    mkdir -p "$2"
+    mkdir -p "$3"
+    while read file; do
+        ~/thesis_SidReed/scripts/modified_get_features.pl -a -t "$file" >| "$2""/"`basename $file`".faa" 2>> "$2""/errors.txt"
+        ~/thesis_SidReed/scripts/modified_get_features.pl -c -t "$file" >| "$3""/"`basename $file`".fna" 2>> "$3""/errors.txt"
+    done < "$1"
 }
 
 main() {
@@ -38,43 +38,45 @@ main() {
     cd -
 }
 
-#need conda environment activated, cant do it in script for some reason so just make sure its activated first
-#$1 is paths to gbff files, $2 is genus name
-shopt -s extglob #allows for globbing in scripts
-main "$1" "$2"
-
-#DEPRECIATED
 extractMixedFastas() {
-    mdir="mix_$2"
-    mkdir -p "$mdir"
+    mkdir -p "$2" #mixed_fastas
     while read file; do
-        ~/thesis_SidReed/scripts/modified_get_features.pl -b -t "$file" >| "$mdir/`basename $file | cut -d'.' -f-2`.fnaa" 2>> "$mdir/errors.txt"
+        ~/thesis_SidReed/scripts/modified_get_features.pl -b -t "$file" >| "$2/`basename $file | cut -d'.' -f-2`.fnaa" 2>> "$2/errors.txt"
     done < "$1"
 }
 
 seperateMixedFastas() {
-    pdir="prot_$1"
-    mkdir -p "$pdir"
-    ndir="nuc_$1"
-    mkdir -p "$ndir"
-    for file in "mix_$1"/*.fnaa; do
+    mkdir -p "$2" #protein_fastas
+    mkdir -p "$3" #nucleotide_fastas
+    for file in "$1"/*.fnaa; do
         python ~/thesis_SidReed/scripts/seperateFasta.py "$file" >| tmp_fnames
         base=`basename $file | cut -d'.' -f-2`
         protname=`tail -1 tmp_fnames`
-        mv "$protname" "./$pdir/$base".faa
+        mv "$protname" "./$2/$base".faa
         nucname=`head -1 tmp_fnames`
-        mv "$nucname" "./$ndir/$base".fna
+        mv "$nucname" "./$3/$base".fna
     done
 }
 
 main2() {
-    extractMixedFastas "$1" "$2"
-    seperateMixedFastas "$2"
-    cat "prot_$2"/*.faa >| all_proteins.faa
+    mkdir -p "$2"
+    cd "$2"
+    mdir="mixed_fastas"
+    extractMixedFastas ../"$1" "$mdir"
+    pdir="protein_fastas"
+    ndir="nucleotide_fastas"
+    seperateMixedFastas "$mdir" "$pdir" "$ndir"
+    cat "$pdir"/*.faa >| all_proteins.faa
     dedupFasta all_proteins.faa
     rmEmptyHeads all_proteins.faa
-    cat "nuc_$2"/*.fna >| all_nucleotides.fna
+    cat "$ndir"/*.fna >| all_nucleotides.fna
     dedupFasta all_nucleotides.fna
     rmEmptyHeads all_nucleotides.fna
 
 }
+
+#need conda environment activated, cant do it in script for some reason so just make sure its activated first
+shopt -s extglob #allows for globbing in scripts
+main2 "$1" "$2" #$1 is file with paths to gbff files, $2 is genus name
+
+#DEPRECIATED

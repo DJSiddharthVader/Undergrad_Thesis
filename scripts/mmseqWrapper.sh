fd8deca29c8ed566b4769f5ce2b2452a2b0f3f95
Sid Reed <sidreed34@gmail.com>
2019-03-04 16:08:43 -0500

    rewrote to be cleaner, redirect output to log files

--- scripts/mmseqWrapper.sh
+++ scripts/mmseqWrapper.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 #$1 is the all proteins fasta file
 #$2 is the genusname
+#variables
 fasta="$1"
-db="mmseq_db_$2"
-out="clusters_$2"
+genus="$2"
+db="mmseq_db_$genus"
+out="clusters_$genus"
+#run mmseq clustering
 mmseqs createdb "$fasta" "$db"
-mmseqs cluster -e 0.05 -c 0.8 --min-seq-id 0.85 --alignment-mode 3 --cluster-mode 1 "$db" "$out" mmseq_tmp
+mmseqs cluster -e 0.05 -c 0.8 --min-seq-id 0.85 --alignment-mode 3 --cluster-mode 1 "$db" "$out" mmseq_tmp >| mmseq.log 2>| mmseq.errors
 mmseqs createtsv "$db" "$db" "$out" "$out".tsv
 \rm -rf mmseqs_tmp
+
+#DEPRECIATED
+##$1 is the all proteins fasta file
+##$2 is the genusname
+#fasta="$1"
+#db="mmseq_db_$2"
+#out="clusters_$2"
+#mmseqs createdb "$fasta" "$db"
+#mmseqs cluster -e 0.05 -c 0.8 --min-seq-id 0.85 --alignment-mode 3 --cluster-mode 1 "$db" "$out" mmseq_tmp >| mmseq.log 2>| mmseq.errors
+#mmseqs createtsv "$db" "$db" "$out" "$out".tsv
+#\rm -rf mmseqs_tmp

fc702fce6100d932ccf748d7e92a724eab2af182
Sid Reed <sidreed34@gmail.com>
2019-03-04 16:10:47 -0500

    trying stuff, testing to get the partitions to work, still getting errors

--- scripts/markophylo_indel_estiamtes.R
+++ scripts/markophylo_indel_estiamtes.R
@@ -3,25 +3,24 @@
 #arg 2 is presenec absence matrix of genefamilies
 #arg 3 is column index json
 #arg 4 is row    index json
-#arg 5 is crispr annotation json
 
 library("ape")
-library("rjson")
 library("markophylo")
+suppressMessages(library("jsonlite"))
 suppressMessages(library("phytools")) #map warnings
 
 #CLI Args
 args <-  commandArgs(trailingOnly=TRUE)
 if (length(args) != 4){
-    stop("args are species_tree.nexus, pa_matrix.txt, column index, row index .n",call.=FALSE)
+    stop("args are species_tree.nexus, pa_matrix.csv, column index, row index .n",call.=FALSE)
 }
 jsonToList <- function(filepath){
-    json <- rjson::fromJSON(paste(readLines(filepath),collapse=""))
+    json <- jsonlite::fromJSON(paste(readLines(filepath),collapse=""))
     lst <- c(sapply(1:length(json)-1,function(x){json[[as.character(x)]]}))
     return(lst)
 }
 jsonToDF <- function(filepath){
-    json <- rjson::fromJSON(paste(readLines(filepath),collapse=""))
+    json <- jsonlite::fromJSON(paste(readLines(filepath),collapse=""))
     df <- do.call("rbind",json)
     df <- t(df)
     return(df)
@@ -35,26 +34,36 @@ readSpeciesTree <- function(filepath){
 }
 readAnnoData <- function(){
     annotation_file <- "/home/sid/thesis_SidReed/pop_annotation_data_frame.json"
-    df <- rjson::fromJSON(paste(readLines(annotation_file),collapse=""))
+    df <- jsonlite::fromJSON(paste(readLines(annotation_file),collapse=""))
     return(df)
 }
-partionByCRISPR <- function(taxlist,annodf){
+partionByCRISPR <- function(spectree,annodf){
     tmp <- t(do.call(rbind,annodf))
+    taxlist <- spectree$tip.label
     accession_col_idx <- grep("Accession Number", colnames(tmp))
     crispr_col_idx <- grep("isCRISPR", colnames(tmp))
-    partitions <- vector("list", 2)
-    names(partitions) <- c("crispr","not_crispr")
+#    partitions <- vector("list", 2)
+#    names(partitions) <- c("crispr","not_crispr")
+    crispr <- vector()
+    not_crispr <- vector()
     for (acc in taxlist){
         rowidx <- match(acc,annodf[[accession_col_idx]])
         iscrispr <- annodf[[crispr_col_idx]][rowidx][[1]]
         if (iscrispr){
-            partitions$crispr <- c(partitions$crispr,acc)
+            crispr <- c(crispr,acc)
         } else {
-            partitions$not_crispr <- c(partitions$not_crispr,acc)
+            not_crispr <- c(not_crispr,acc)
         }
     }
-    print(partitions)
-    return(partitions)
+    crispr <- c(crispr,6:length(taxlist))
+    partitions <- list(crispr,not_crispr)
+    if (length(partitions[[1]]) == 0){
+        return(list(1:length(taxlist),ancestral))
+    } else if (length(partitions[[2]]) == 0){
+        return(list(1:length(taxlist),ancestral))
+    } else {
+        return(partitions)
+    }
 }
 main <- function(args){
     #read species tree
@@ -62,23 +71,23 @@ main <- function(args){
     #read P/A matrix, name cols/rows
     pamat <- read.csv(args[2],header=FALSE)
     pamat <- t(pamat)
-    colnames(pamat) <- jsonToList(args[3])
-    row.names(pamat) <- jsonToList(args[4])
+    row.names(pamat) <- jsonToList(args[3])
+    colnames(pamat) <- jsonToList(args[4])
     #create crispr tip partitions
     annodf <- readAnnoData()
-    crispr_tip_partition <- partionByCRISPR(species_tree$tip.label,annodf)
-    print(length(crispr_tip_partition$crispr))
-    print(length(crispr_tip_partition$not_crispr))
+    crispr_tip_partition <- partionByCRISPR(species_tree,annodf)
     #markophylo
     estrates <- markophylo::estimaterates(usertree=species_tree,
                                          userphyl=pamat,
                                          alphabet=c(0,1),
                                          bgtype="listofnodes",
                                          bg=crispr_tip_partition,
-                                         modelmat="BDSYM",
                                          rootprob="maxlik",
+                                         modelmat="BDSYM",
                                          matchtiptodata=TRUE)
-    #print(estrates)
-    #write markophylo results to csv
+    #write to file
+    sink("markophylo_results.txt")
+    print(estrates)
+    sink()
 }
 main(args)

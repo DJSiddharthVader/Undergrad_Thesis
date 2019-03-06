4f4d8502eb6cb47dc01f332473253463b28ebcdf
Sid Reed <sidreed34@gmail.com>
2019-03-05 19:41:34 -0500

    reflects changes in fixing species tree, gene tree scripts

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scriptdir="/home/sid/thesis_SidReed/scripts"
-#fps="$1" #file with abs paths to all genomes being used
+crispr_annotation_info="/home/sid/thesis_SidReed/data/pop_annotation_data_frame.json"
 genus="$1" #genus name, capitalize first lettter
 echo "fetching/formatting sequences"
 mkdir -p "$genus"
@@ -11,11 +11,9 @@ python "$scriptdir"/shortenFastaheaders.py nucleotide_fastas "$genus"
 cat nucleotide_fastas/*.fna >| all_nucleotides.fna
 #python "$scriptdir"/dedupFasta.py all_nucleotides.fna
 #protein
-python "$scriptdir"/shortenFastaheaders.py protein_fastas "$genus"
+python "$scriptdir"/shortenFastaheaders.py protein_fastas
 cat protein_fastas/*.faa >| all_proteins.faa
 #python "$scriptdir"/dedupFasta.py all_proteins.faa
-#echo "preping sequences"
-#"$scriptdir"/prepSeqs.sh "$fps" "$genus"
 echo "single link clustering to gene families"
 mkdir -p ./mmseq_clustering
 cd ./mmseq_clustering
@@ -25,12 +23,17 @@ python "$scriptdir"/parse_mmseq_clusters.py ./mmseq_clustering/clusters_"$genus"
 echo "creating PA matrix"
 python "$scriptdir"/createPAMatrix.py gene_families.json protein_fastas/ "$genus"
 echo "building species trees"
-python "$scriptdir"/build_16s_species_tree.py all_nucleotides.fna "$genus"_fastas_info.json gene_families.json
+python "$scriptdir"/build_16S_species_tree_from_families.py all_nucleotides.fna fasta_headers_info.json
+if [ "$?" -neq 0 ]; then
+    echo "Issue with building species tree, aborting"
+    exit 1
+fi
 echo "building gene trees"
-python "$scriptdir"/generate_gene_trees.py gene_families.json all_nucleotides.fna "$genus"
+python "$scriptdir"/generate_gene_trees.py gene_families.json all_nucleotides.fna
 exit 1
 echo "running markophylo"
-Rscript "$scriptdir"/markophylo_indel_estimates.R "species_tree_files/species_tree_$genus/species_tree_$genus.con.tre" binary_pa_matrix.csv column_indexes_families.json row_organism_idxs.json
+Rscript "$scriptdir"/markophylo_indel_estimates.R
+"species_tree_files/species_tree_$genus/species_tree_$genus.con.tre" binary_pa_matrix.csv column_indexes_families.json row_organism_idxs.json "$crispr_annotation_info"
 echo "building  network"
 python "$scriptdir"/build_network_from_trees.py ../"$genus"
 

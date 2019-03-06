e9c6074c8da811fe792de9f15a85ed4ccdca97af
Sid Reed <sidreed34@gmail.com>
2019-03-05 19:35:51 -0500

    for organizing gene tree files

---
+++ scripts/renameGeneTreeFiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+fam="$1"
+fdir="gene_tree_files/trees/$fam"
+mkdir -p "$fdir"
+mv "$fam"_* \
+   "$fam".mcmc \
+   "$fam".parts \
+   "$fam".con.tre \
+   "$fam".run1.p \
+   "$fam".run1.t \
+   "$fam".run2.p \
+   "$fam".run2.t \
+   "$fam".trprobs \
+   "$fam".tstat \
+   "$fam".vstat \
+   "$fdir"

1d84d498d9cf49a71887a7b1dd7d79a3761d5453
Sid Reed <sidreed34@gmail.com>
2019-03-14 16:25:58 -0400

    now works, outputs bootstrap sample networks

--- scripts/network_builder.R
+++ scripts/network_builder.R
@@ -1,4 +1,7 @@
+#!/usr/bin/env Rscript
 library("ape")
+library('progress')
+suppressMessages(library('igraph')) #masking
 suppressMessages(library("phangorn")) #map warnings
 
 set.seed(9)
@@ -27,99 +30,186 @@ set.seed(9)
 #                  fam6.newick
 #                  ...
 #        ...
-#    networks_raw/all_gene_trees.txt
-#                 subset_01.txt
-#                 ...
-#    networks_json/all_gene_trees.json
-#                  subset_01.json
-#                  ...
+#    raw_hide/subset_01.hide
+#             subset_02.hide
+#             subset_03.hide
+#             ...
+#    ncols/subset_1.ncol
+#          subset_2.ncol
+#          subset_3.ncol
+#          subset_4.ncol
+#          ...
 
+makepb <- function(total){
+    pb <- progress_bar$new(format = "[:bar] :percent eta: :eta",total = total, clear = FALSE, width= 75)
+    return(pb)
+}
 setUpDirs <- function(rootDir){
-    netbase = paste(rootDir,'networkfiles',sep='/')
-    mkdir(netbase)
-    mkdir(paste(netbase,'all_newick_trees',sep='/'))
-    mkdir(paste(netbase,'subsets',sep='/'))
+    netbase = paste(rootDir,'network_files',sep='/')
+    dir.create(netbase)
+    dir.create(paste(netbase,'all_newick_trees',sep='/'))
+    dir.create(paste(netbase,'subsets',sep='/'))
+    dir.create(paste(netbase,'raw_hide',sep='/'))
+    dir.create(paste(netbase,'csvs',sep='/'))
 }
 
 convertToNwk <- function(pathToNexus,isSpecies=FALSE){
-    #newbase = .../genusname/
     tree <- ape::read.nexus(file=pathToNexus)
+    if (!is.rooted(tree)){
+        tree <- phangorn::midpoint(tree)
+    }
+    tree$edge.length <- NULL
+    if (!is.binary.tree(tree)){
+        tree <- multi2di(tree)
+    }
     if (isSpecies){
-        nwkpath <- 'networkfiles/all_newick_trees/sepcies.newick'
+        nwkpath <- 'network_files/all_newick_trees/species.newick'
     } else{
-        nwkpath <- paste('networkfiles/all_newick_trees/',strsplit(basename(pathToNexus),'.')[1],'.newicl',sep='')
+        filename <- strsplit(basename(pathToNexus),'.',fixed=TRUE)[[1]][1]
+        nwkpath <- paste('network_files/all_newick_trees/',filename,'.newick',sep='')
     }
     ape::write.tree(tree,nwkpath)
+    return(nwkpath)
 }
 
-
-main <- function(rootDir){
-    setUpDirs(rootDir)
-    speciesTreePath <- paste(rootDir,'species_tree_files/species_tree/rooted_species_tree.con.tre',sep=''
-    convertToNwk(speciesTreePath,isSpecies=TRUE)
-    geneTreeFiles <- list.files(paste(rootDir,'gene_tree_files/trees/',sep=''),pattern='**/*.con.tre',full.names=TRUE)
-    print(geneTreeFiles)
-}
-
-#Markophylo Functions
-newfp <- function(fp){
-    base <- basename(fp)
-    newbase <- paste('rooted_',base,sep='')
-    newbasenwk <- paste('rooted_',base,'.newick',sep='')
-    dir <- dirname(fp)
-    newfp <- paste(dir,newbase,sep='/')
-    newfpnwk <- paste(dir,newbasenwk,sep='/')
-    return(c(newfp,newfpnwk))
-}
-readSpeciesTree <- function(filepath){
-    spt <- ape::read.nexus(file=filepath)
-    rspt <- phangorn::midpoint(spt)
-    if (!is.binary.tree(rspt)){
-        rspt <- multi2di(rspt)
+generateSample <- function(diridx,allGeneTrees,size){
+    sdir <- paste('network_files/subsets/subset_',diridx,sep='')
+    dir.create(sdir)
+    file.copy('network_files/all_newick_trees/species.newick',paste(sdir,'species.newick',sep='/'))
+    sample <- sample(allGeneTrees,size=size,replace=FALSE)
+    for (file in sample){
+        file.copy(file,paste(sdir,basename(file),sep='/'))
     }
-    rootfp <- newfp(filepath)
-    ape::write.nexus(rspt,file=rootfp[1])
-    ape::write.tree(rspt,file=rootfp[2])
-    rspt <- ape::read.tree(file=rootfp[2])
-    return(rspt)
 }
-loadAnnotationInfo <- function(filepath){
-    crisprAnnotation <- fromJSON(filepath)
-    crisprAnnotation['Cas Proteins (CRISPRone)'] <- NULL
-    return(crisprAnnotation)
+sampleSet <- function(){
+    allsubsets <- c()
+    subsets <- list.files('network_files/subsets',full.names=TRUE)
+    for (set in subsets){
+        allsubsets <- c(allsubsets,list.files(set,pattern='*.newick'))
+    }
+    allsubsets <- unique(allsubsets)
+    print(length(allsubsets))
+    alltrees <- list.files('network_files/all_newick_trees',pattern='*.newick')
+    print(length(alltrees))
+    return(setequal(alltrees,allsubsets))
 }
-trimVersion <- function(accnum){
-    trimmed <- strsplit(accnum,'.',fixed=TRUE)[[1]][1]
-    return(trimmed)
+sampleUntillAllUsed <- function(size){
+    stopsampling <- sampleSet()
+    print(stopsampling)
+    if (stopsampling){
+        return(NULL)
+    }
+    subsetidx <- length(list.files('network_files/subsets'))+1
+    allGeneTrees <- list.files('network_files/all_newick_trees',full.names=TRUE)
+    allGeneTrees <- allGeneTrees[!grepl('species.newick',allGeneTrees)]
+    while (!stopsampling){
+        generateSample(subsetidx,allGeneTrees,size)
+        stopsampling <- sampleSet()
+        subsetidx <- subsetidxs + 1
+    }
 }
-partitionByCRISPR <- function(spt,crisprAnnotation){
-    #annotation info
-    tmp <- t(do.call(rbind,crisprAnnotation))
-    accession_col_idx <- grep("Accession Number", colnames(tmp))
-    crispr_col_idx <- grep("isCRISPR", colnames(tmp))
-    #tree nodes
-    taxlist <- spt$tip.label
-    root <- getRoot(spt)
-    #create tip partitions
-    crispr <- c()
-    not_crispr <- c()
-    for (acc in taxlist){
-        rowidx <- match(trimVersion(acc),crisprAnnotation[[accession_col_idx]])
-        iscrispr <- crisprAnnotation[[crispr_col_idx]][rowidx][[1]]
-        nodenum <- match(acc,taxlist)
-        pathToRoot <- nodepath(spt,from=nodenum,to=root)
-        if (iscrispr){
-            crispr <- c(crispr,pathToRoot)
-        } else {
-            not_crispr <- c(not_crispr,pathToRoot)
+runHide <- function(treedir){
+    lua <- '/usr/bin/lua'
+    hideexe <- '/home/sid/thesis_SidReed/hide_program/score.lua'
+    edgelist <- system(paste(lua,hideexe,treedir),intern=TRUE,ignore.stderr=TRUE)
+    outf <- file(paste('network_files/raw_hide/',basename(treedir),'.hide',sep=''))
+    writeLines(edgelist,outf)
+    close(outf)
+}
+normalizeEdge <- function(weight,fname){
+    subset <- strsplit(basename(fname),'.',fixed=TRUE)[[1]][1][1]
+    subdir <- paste('network_files/subsets/',subset,sep='')
+    norm <- length(list.files(subdir))-1
+    return(weight/norm)
+}
+parseEdge <- function(line,hidefile){
+    split <- strsplit(line,'\t')
+    weight <- as.numeric(split[[1]][1])
+    if (weight == 0){
+       return(c(-1,-1,-1))
+    }
+    esplit <- strsplit(split[[1]][2],' ')
+    source <- strsplit(esplit[[1]][1],'-')[[1]][2]
+    sink <- strsplit(esplit[[1]][3],'-')[[1]][2]
+    nweight <- normalizeEdge(weight,hidefile)
+    direction <- split[[1]][3]
+    edge <- list(source=source,sink=sink,raw_weight=weight,weight=nweight,direction=direction)
+    return(edge)
+}
+hideToEdgeList <- function(hidefile){
+    lines <- readLines(hidefile)
+    parsed_edge_list <- parseEdge(lines[1],hidefile)
+    for (line in lines[2:length(lines)]){
+        edge <- parseEdge(line,hidefile)
+        if (edge[3][1] == -1){
+            next
         }
+        parsed_edge_list <- rbind(parsed_edge_list,edge)
     }
-    #check partition not empty and dedup
-    if (length(crispr) == 0 || length(not_crispr) == 0){
-        allnodes <- 1:(length(spt$tip.label)+spt$Nnode)
-        partitions <- list(non_crispr=allnodes) #all nodes in tree
-    } else {
-        partitions <- list(crispr=unique(crispr),non_crispr=unique(not_crispr))
-    }
-    return(partitions)
+    return(parsed_edge_list)
 }
+alltreenetwork <- function(){
+    return(1)
+}
+combineNetworks <- function(graphcsvs){
+    glist <- c()
+}
+
+main <- function(rootDir,num,size){
+    setUpDirs(rootDir)
+    print('converting trees to newick')
+    #convert species tree to newick
+    speciesTreePath <- paste(rootDir,'species_tree_files/species_tree/species_tree.con.tre',sep='')
+    speciesTreePath <- convertToNwk(speciesTreePath,isSpecies=TRUE)
+    #convert gene tree to newick
+    dirlist <- list.files(paste(rootDir,'gene_tree_files/trees',sep=''),pattern='fam*',full.names=TRUE)
+    geneTreePaths <- c()
+    pb <- makepb(length(dirlist))
+    for (dir in dirlist){
+        tree <- list.files(dir,patter='*.con.tre',full.names=TRUE)
+        gpath <- convertToNwk(tree[1])
+        geneTreePaths <- c(geneTreePaths,gpath)
+        pb$tick()
+    }
+    #generate samples of gene trees
+    print('generating samples')
+    allGeneTrees <- list.files('network_files/all_newick_trees',full.names=TRUE)
+    allGeneTrees <- allGeneTrees[!grepl('species.newick',allGeneTrees)]
+    pb <- makepb(num)
+    for (s in 1:num){
+        generateSample(s,allGeneTrees,size)
+        pb$tick()
+    }
+    #sampleUntillAllUsed(size)
+    #build networks
+    print('running HiDe')
+    subsets <- list.files('network_files/subsets',full.names=TRUE)
+    pb <- makepb(length(subsets))
+    for (sample in subsets){
+        runHide(sample)
+        pb$tick()
+    }
+    #parse and write networks
+    print('parsing and writing networks')
+    hidefiles <- list.files('network_files/raw_hide',full.names=TRUE)
+    pb <- makepb(length(hidefiles))
+    for (hide in hidefiles){
+        edgelist <- hideToEdgeList(hide)
+        base <- strsplit(basename(hide),'.',fixed=TRUE)[[1]][1]
+        outf <- paste('network_files/csvs/',base,'.csv',sep='')
+        write.table(edgelist,file=outf,sep='~',row.names=1:dim(edgelist)[1])
+        pb$tick()
+    }
+}
+
+
+#if name == main
+if (sys.nframe() == 0){
+    #CLI Args
+    args <-  commandArgs(trailingOnly=TRUE)
+    if (length(args) != 1){
+        stop("arg is genus dir .n",call.=FALSE)
+    }
+    main(args[1],1000,50)
+}
+

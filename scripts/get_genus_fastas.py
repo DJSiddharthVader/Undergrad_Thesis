6d82877f987a33f1ecd967453d524c532c609dd0
Sid Reed <sidreed34@gmail.com>
2019-03-17 16:33:19 -0400

    reworked for new master df

--- scripts/get_genus_fastas.py
+++ scripts/get_genus_fastas.py
@@ -4,45 +4,51 @@ import sys
 import json
 import pandas as pd
 
-#suffixes and directories
-nuc_suffix = "_cds_from_genomic.fna.gz"
-source_nuc_dir = "/home/sid/thesis_SidReed/data/nuc_cds_genomic"
-prot_suffix = "_translated_cds.faa.gz"
-source_prot_dir = "/home/sid/thesis_SidReed/data/prot_cds_genomic"
-df_path = '/home/sid/thesis_SidReed/data/pop_annotation_data_frame.json'
+#df_path = '/home/sid/thesis_SidReed/data/fasta_crispr_annotation_df.json'
 
-#args
-genus = sys.argv[1]
-if len(sys.argv) < 2:
-    destdir = sys.argv[2]
-else:
-    destdir = os.getcwd()
+def mkdirs(destdir):
+    nucdir = "{}/nucleotide_fastas".format(destdir)
+    os.system("mkdir -p {}".format(nucdir))
+    protdir = "{}/protein_fastas".format(destdir)
+    os.system("mkdir -p {}".format(protdir))
+    return(nucdir,protdir)
 
-#creatdirs
-nucdir = "{}/nucleotide_fastas".format(destdir)
-os.system("mkdir -p {}".format(nucdir))
-protdir = "{}/protein_fastas".format(destdir)
-os.system("mkdir -p {}".format(protdir))
-
-#Copy,Rename fasta files
-meta_data_df = pd.DataFrame(json.load(open(df_path)))
-genus_df = meta_data_df[meta_data_df['Genus'] == genus]
-print(len(genus_df))
-for i,row in genus_df.iterrows():
-    filename = 'GCF_' + '_'.join(row['GCA'].split('_')[1:])
-    cpnuc = "cp {}/{}{} {}/{}{}".format(source_nuc_dir,
-                                        filename,
-                                        nuc_suffix,
-                                        nucdir,
-                                        row['Accession Number'],
-                                        nuc_suffix)
-    os.system(cpnuc)
-    cpprot = "cp {}/{}{} {}/{}{}".format(source_prot_dir,
-                                        filename,
-                                        prot_suffix,
+def copyfiles(df_path,genus,nucdir,protdir):
+    nuc_suffix = "_cds_from_genomic.fna.gz"
+    prot_suffix = "_translated_cds.faa.gz"
+    meta_data_df = pd.DataFrame(json.load(open(df_path)))
+    genus_df = meta_data_df[meta_data_df['Genus'] == genus]
+    for i,row in genus_df.iterrows():
+        cpnuc = "cp {} {}/{}{}".format(row['nucPath'],
+                                       nucdir,
+                                       row['Accession Number'],
+                                       nuc_suffix)
+        os.system(cpnuc)
+        cpprot = "cp {} {}/{}{}".format(row['protPath'],
                                         protdir,
                                         row['Accession Number'],
                                         prot_suffix)
-    os.system(cpprot)
-os.system("gzip -d {}/*.gz".format(nucdir))
-os.system("gzip -d {}/*.gz".format(protdir))
+        os.system(cpprot)
+    os.system("gzip -d {}/*.gz".format(nucdir))
+    os.system("gzip -d {}/*.gz".format(protdir))
+
+def main(genus,df_path,destdir):
+    nucdir,protdir = mkdirs(destdir)
+    copyfiles(df_path,genus,nucdir,protdir)
+    return None
+
+if __name__ == '__main__':
+    genus = sys.argv[1]
+    df_path = sys.argv[2]
+    if len(sys.argv) < 3:
+        destdir = sys.argv[3]
+    else:
+        destdir = os.getcwd()
+    main(genus,df_path,destdir)
+
+
+
+#DEPRECIATED
+#suffixes and directories
+#source_nuc_dir = "/home/sid/thesis_SidReed/data/nuc_cds_genomic"
+#source_prot_dir = "/home/sid/thesis_SidReed/data/prot_cds_genomic"

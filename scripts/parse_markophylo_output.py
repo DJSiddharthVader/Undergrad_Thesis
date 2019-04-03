82fd52f825665051d85355c75566e9ae64b46f4a
Sid Reed <sidreed34@gmail.com>
2019-04-03 06:31:38 -0400

    updated to work with markophylo changes, no se due to issues sometimes

--- scripts/parse_markophylo_output.py
+++ scripts/parse_markophylo_output.py
@@ -22,38 +22,39 @@ def parse2(filepath,genus):
     ratelist = lines[lines.index('$rates')+4].split(' ')[1:]
     ratelist = [x for x in ratelist if x != '']
     rates = [float(x) for x in ratelist]
-    if len(rates) == 2:
-        crate,ncrate = rates
-    else:
-        crate, ncrate = 0,rates[0]
-    #std errs
-    semlist = lines[lines.index('$se$rates')+4].split(' ')[1:]
-    semlist = [x for x in semlist if x != '']
-    if len(semlist) == 2:
-        csem,ncsem = semlist
-    else:
-        csem,ncsem = 0,semlist[0]
-    return {'genus':genus,
-            'crispr_indel_rate':crate,
-            'non-crispr_indel_rate':ncrate,
-            'crispr_indel_sem':csem,
-            'non-crispr_indel_sem':ncsem}
+    if len(rates) != 3:
+        rates = [0].extend(rates)
+        print(rates)
+    crate,ncrate,intrate = rates
+    try:
+        #std errs
+        semlist = lines[lines.index('$se$rates')+4].split(' ')[1:]
+        semlist = [x for x in semlist if x != '']
+        if len(semlist) != 3:
+            semlist = [0].extend(semlist)
+            print(semlist)
+        csem,ncsem,intsem = semlist
+        return {'genus':genus,
+                'crispr_indel_rate':crate,
+                'non-crispr_indel_rate':ncrate,
+                'internal_indel_rate':intrate,
+                'crispr_indel_sem':csem,
+                'non-crispr_indel_sem':ncsem,
+                'internal_indel_sem':ncsem}
+    except ValueError:
+        return {'genus':genus,
+                'crispr_indel_rate':crate,
+                'non-crispr_indel_rate':ncrate,
+                'internal_indel_rate':intrate}
+
 
 if __name__ == '__main__':
-    if len(sys.argv) > 2:
-        basedir = sys.argv[1]
-        genus = os.path.basename(basedir)
-        inpath = os.path.join(os.getcwd(),basedir,'markophylo_results.txt')
-        outd = parse2(inpath,genus)
-        outpath = os.path.join(basedir,'{}_markophylo_results.json'.format(genus))
-        json.dump(outd,open(outpath,'w'))
-    else:
-        basedir = sys.argv[1]
-        genus = os.path.basename(basedir)
-        inpath = os.path.join(os.getcwd(),basedir,'markophylo_results.txt')
-        outd = parse(inpath,genus)
-        outpath = os.path.join(basedir,'{}_markophylo_results.json'.format(genus))
-        json.dump(outd,open(outpath,'w'))
+    basedir = sys.argv[1]
+    genus = os.path.basename(basedir)
+    inpath = os.path.join(os.getcwd(),basedir,'markophylo_results.txt')
+    outd = parse2(inpath,genus)
+    outpath = os.path.join(basedir,'{}_markophylo_results.json'.format(genus))
+    json.dump(outd,open(outpath,'w'))
 
 #for dir in `ls | grep -v -E "missing*|notEn*|\.|noCRISPR*" `; do python ../../scripts/parse_markophylo_output.py "$dir"; done
 #find genus_data -maxdepth 2 -name "*markophylo_results.json" -exec cp {} marko_reports/ \;
